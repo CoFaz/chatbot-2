185d6bcabd2c79e95c98e7685418b1a8573ae890
CoFaz <97194720+CoFaz@users.noreply.github.com>
2024-07-31 10:46:01 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 # FastAPI endpoint
-FASTAPI_URL = "http://127.0.0.1:8000/process"
+FASTAPI_URL = "http://127.0.0.1:8000/"
 
 # Function to send user input to FastAPI and get response
 def get_response_from_backend(prompt):
